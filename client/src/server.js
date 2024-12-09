ec4afd9685ae3c775942454a3066922248b16923
mna <mn45994445@gmail.com>
2024-12-09 16:57:07 +0500

    final

--- client/src/server.js
+++ client/src/server.js
@@ -1,2 +1,2 @@
-// export const SERVER = "https://registration-authserver.vercel.app";
-export const SERVER = "http://localhost:5000";
+export const SERVER = "https://registration-authserver.vercel.app";
+// export const SERVER = "http://localhost:5000";
